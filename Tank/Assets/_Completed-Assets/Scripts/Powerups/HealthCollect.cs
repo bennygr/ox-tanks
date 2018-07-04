df61bbdf8761619a6886bcf0aabf05d08887c5f3
Benjamin Grüdelbach <benjamin.gruedelbach@open-xchange.com>
2018-07-04 13:10:37 +0200

    First Health powerup logic

---
+++ Tank/Assets/_Completed-Assets/Scripts/Powerups/HealthCollect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Complete;
+using UnityEngine;
+namespace Complete
+{
+    public class HealthCollect : MonoBehaviour
+    {
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var health = other.GetComponent<TankHealth>();
+            if (health != null)
+            {
+                health.Heal(25);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
