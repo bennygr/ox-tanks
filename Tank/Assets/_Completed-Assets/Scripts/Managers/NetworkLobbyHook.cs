b705110b76954e178de8a2846acee759ef21dd32
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-07-04 17:05:30 +0200

    primitive lobby functionality

---
+++ Tank/Assets/_Completed-Assets/Scripts/Managers/NetworkLobbyHook.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Complete {
+	public class NetworkLobbyHook : Prototype.NetworkLobby.LobbyHook {
+		public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer) {
+			if (lobbyPlayer == null) {
+				return;
+			}
+
+			Prototype.NetworkLobby.LobbyPlayer lp = lobbyPlayer.GetComponent<Prototype.NetworkLobby.LobbyPlayer>();
+
+			if (lp != null) {
+				NetworkGameManager.AddPlayer(gamePlayer, lp.slot, lp.playerColor, lp.nameInput.text);
+			}
+		}
+	}
+}
