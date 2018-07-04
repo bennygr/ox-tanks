cf84384b485bc9201d859d51c011a60e36d7cf87
Benjamin Grüdelbach <benjamin.gruedelbach@open-xchange.com>
2018-07-04 13:24:31 +0200

    Health box: Bounding box and collision trigger

--- Tank/Assets/_Completed-Assets/Scripts/Powerups/HealthCollect.cs
+++ Tank/Assets/_Completed-Assets/Scripts/Powerups/HealthCollect.cs
@@ -6,13 +6,13 @@ namespace Complete
 {
     public class HealthCollect : MonoBehaviour
     {
-
+        public float increaseHealth = 25f;
         private void OnTriggerEnter(Collider other)
         {
             var health = other.GetComponent<TankHealth>();
             if (health != null)
             {
-                health.Heal(25);
+                health.Heal(increaseHealth);
             }
             Destroy(gameObject);
         }
