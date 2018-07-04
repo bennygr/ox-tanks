850c96ae0ac927505820f76114f5625321d75ff6
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-07-04 13:02:14 +0200

    correctly register and sync health bars accross all clients

--- Tank/Assets/_Completed-Assets/Scripts/Tank/NetworkTankHealth.cs
+++ Tank/Assets/_Completed-Assets/Scripts/Tank/NetworkTankHealth.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 namespace Complete {
-	public class NetworkTankHealth : MonoBehaviour {
+	public class NetworkTankHealth : NetworkBehaviour {
 		public float m_StartingHealth = 100f;               // The amount of health each tank starts with.
 		public Slider m_Slider;                             // The slider to represent how much health the tank currently has.
 		public Image m_FillImage;                           // The image component of the slider.
@@ -13,7 +14,9 @@ namespace Complete {
 
 		private AudioSource m_ExplosionAudio;               // The audio source to play when the tank explodes.
 		private ParticleSystem m_ExplosionParticles;        // The particle system the will play when the tank is destroyed.
-		private float m_CurrentHealth;                      // How much health the tank currently has.
+
+		[SyncVar(hook = "SetHealthUI")]
+		public float m_CurrentHealth;                      // How much health the tank currently has.
 		private bool m_Dead;                                // Has the tank been reduced beyond zero health yet?
 
 
@@ -33,18 +36,15 @@ namespace Complete {
 			// When the tank is enabled, reset the tank's health and whether or not it's dead.
 			m_CurrentHealth = m_StartingHealth;
 			m_Dead = false;
-
-			// Update the health slider's value and color.
-			SetHealthUI();
 		}
 
 
 		public void TakeDamage(float amount) {
+			if (!isServer) {
+				return;
+			}
 			// Reduce current health by the amount of damage done.
 			m_CurrentHealth -= amount;
-
-			// Change the UI elements appropriately.
-			SetHealthUI();
 
 			// If the current health is at or below zero and it has not yet been registered, call OnDeath.
 			if (m_CurrentHealth <= 0f && !m_Dead) {
@@ -53,12 +53,11 @@ namespace Complete {
 		}
 
 
-		private void SetHealthUI() {
+		void SetHealthUI(float currentHealth) {
 			// Set the slider's value appropriately.
-			m_Slider.value = m_CurrentHealth;
-
+			m_Slider.value = currentHealth;
 			// Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
-			m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
+			m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, currentHealth / m_StartingHealth);
 		}
 
 
