b10ba8ff2d06b8f161c29f7b4ffb3628df220b44
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-07-04 09:40:37 +0200

    orthographic chase cam

--- Tank/Assets/_Completed-Assets/Scripts/Camera/ChaseCamera.cs
+++ Tank/Assets/_Completed-Assets/Scripts/Camera/ChaseCamera.cs
@@ -4,32 +4,21 @@ using UnityEngine;
 
 public class ChaseCamera : MonoBehaviour {
 
-	public Transform lookAt;
-	public Transform camTransform;
+	public Transform target;            // The position that that camera will be following.
+	public float smoothing = 5f;        // The speed with which the camera will be following.
 
-	private Camera cam;
+	Vector3 offset;                     // The initial offset from the target.
 
-	private float distance = 10.0f;
-	private float currentX = 0.0f;
-	private float currentY = 0.0f;
-	private float sensitivityX = 4.0f;
-	private float sensitivityY = 1.0f;
-
-    /// <summary>
-    /// Initialise the camera's transform and pick up the main camera
-    /// </summary>
-	private void Start() {
-		camTransform = transform;
-		cam = Camera.main;
+	void Start() {
+		// Calculate the initial offset.
+		offset = transform.position - target.position;
 	}
 
-    /// <summary>
-    /// Positions the camera behind the player.
-    /// </summary>
-	private void LateUpdate() {
-		Vector3 direction = new Vector3(0, 0, -distance);
-		Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-		camTransform.position = lookAt.position + rotation * direction;
-	}
+	void FixedUpdate() {
+		// Create a postion the camera is aiming for based on the offset from the target.
+		Vector3 targetCamPos = target.position + offset;
 
+		// Smoothly interpolate between the camera's current position and it's target position.
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
 }
