02c6c26e5bc6a0c7a077b0590706c2c29ae7f6f5
Benjamin Grüdelbach <benjamin.gruedelbach@open-xchange.com>
2018-07-04 15:08:59 +0200

    Speed powerups

---
+++ Tank/Assets/_Completed-Assets/Scripts/Powerups/SpeedCollect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Complete;
+using UnityEngine;
+
+namespace Complete
+{
+    public class SpeedCollect : AbstractPowerUp
+    {
+        public float speedFactor = 2f;
+        public float time = 0f;
+        public float maxSpeed = 24f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var movement = other.GetComponent<TankMovement>();
+            if (movement != null)
+            {
+                movement.SetSpeedFactor(speedFactor, time, maxSpeed);
+            }
+
+            PowerUpManager.CleanSpawningPoint(gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
