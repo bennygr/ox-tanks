1c351a138685f456a084d22d21afb14baef37a4f
Jan-Oliver Huhn <jan-oliver.huhn@open-xchange.com>
2018-07-05 10:59:24 +0200

    added destructible tables

---
+++ Tank/Assets/OXTanksFurniture/Scripts/FurnitureHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureHealth : AbstractGameHealth {
+
+    private void Awake()
+    {
+        base.Init();
+    }
+
+    public void TakeDamage(float amount)
+    {
+        base.ApplyDamage(amount);
+        base.CalculateDeath();
+    }
+
+    private void OnDeath()
+    {
+        base.ApplyDeath();
+    }
+
+    private void OnEnable()
+    {
+        base.Enable();
+    }
+
+}
