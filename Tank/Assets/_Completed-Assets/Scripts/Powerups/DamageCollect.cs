b3fde0455e0c26a6f12581c7484da34e1ee7ceb9
Benjamin Grüdelbach <benjamin.gruedelbach@open-xchange.com>
2018-07-06 09:16:27 +0200

    Damage powerup

---
+++ Tank/Assets/_Completed-Assets/Scripts/Powerups/DamageCollect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Complete;
+using UnityEngine;
+
+namespace Complete
+{
+
+    public class DamageCollect : AbstractPowerUp
+    {
+        public Rigidbody m_DamageSign;
+        public float damageFactor = 2f;
+        public float time = 10f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var shooting = other.GetComponent<TankShooting>();
+            if (shooting != null)
+            {
+                shooting.IncreaseDamage(damageFactor, time, m_DamageSign);
+            }
+
+            PowerUpManager.CleanSpawningPoint(gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
--- Tank/Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
+++ Tank/Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
@@ -6,7 +6,7 @@ namespace Complete
     public class TankShooting : MonoBehaviour
     {
         public int m_PlayerNumber = 1;              // Used to identify the different players.
-        public Rigidbody m_Shell;                   // Prefab of the shell.
+        public Rigidbody m_Shell;                   // Prefab of the shell.       
         public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
         public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
         public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
@@ -17,12 +17,15 @@ namespace Complete
         public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
 
 
-
         private string m_FireButton;                // The input axis that is used for launching shells.
         private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
         private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
         private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+
         private float m_DamageMultiplier = 1f;
+        private System.DateTime damageChanged;
+        private float damageTime;
+        private Rigidbody powerupSign;
 
 
         private void OnEnable()
@@ -45,6 +48,22 @@ namespace Complete
 
         private void Update()
         {
+            if (powerupSign != null)
+            {
+                powerupSign.transform.position = m_FireTransform.position + new Vector3(0, 10, 0);
+                powerupSign.transform.rotation = m_FireTransform.rotation;
+            }
+
+            //Check if damage multiplier is expired
+            if ((System.DateTime.Now - this.damageChanged).Seconds > this.damageTime && this.damageChanged != System.DateTime.MinValue)
+            {
+                this.m_DamageMultiplier = 1f;
+                Debug.Log("Damage powerup for player " + m_PlayerNumber + " is over.");
+                this.damageChanged = System.DateTime.MinValue;
+                Destroy(powerupSign.gameObject);
+                powerupSign = null;
+            }
+
             // The slider should have a default value of the minimum launch force.
             m_AimSlider.value = m_MinLaunchForce;
 
@@ -79,6 +98,22 @@ namespace Complete
             {
                 // ... launch the shell.
                 Fire();
+            }
+        }
+
+        public void IncreaseDamage(float factor, float time, Rigidbody damageSign)
+        {
+            if (this.m_DamageMultiplier == 1f)
+            {
+                Debug.Log("Increased damage with factor " + factor + " for player " + m_PlayerNumber);
+                this.m_DamageMultiplier = factor;
+                this.damageChanged = System.DateTime.Now;
+                this.damageTime = time;
+                this.powerupSign =
+                    Instantiate(damageSign,
+                                m_FireTransform.position + new Vector3(0, 10, 0),
+                                m_FireTransform.rotation) as Rigidbody;
+
             }
         }
 
