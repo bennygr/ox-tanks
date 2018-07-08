ff0c85ba847555ca186a1918aa830766fb5b87a5
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-07-08 18:46:39 +0200

    increased turning speed

--- OX Tanks/Assets/Scripts/Movement/TankMovement.cs
+++ OX Tanks/Assets/Scripts/Movement/TankMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 /// <summary>
 /// A simple tank movement script to controll the tank. It should
@@ -11,7 +12,7 @@ public class TankMovement : MonoBehaviour {
 	// Initial moving speed
 	public float movingSpeed = 5f;
 	// Initial turning speed
-	public float turningSpeed = 20f;
+	public float turningSpeed = 100f;
 	// Initial speed multiplier (in case a speed buff is applied)
 	public float speedMultiplier = 1f;
 
