03824c6573d72dd1dd3f669196533f87d7f2ff85
Ioannis Chouklis <jhouklis@gmail.com>
2018-07-10 19:07:52 +0200

    made the player rig prefab readonly

--- OX Tanks/Assets/Scripts/Spawning/SpawnManager.cs
+++ OX Tanks/Assets/Scripts/Spawning/SpawnManager.cs
@@ -9,7 +9,7 @@ public class SpawnManager : MonoBehaviour {
 	private List<GameObject> tankPrefabs = new List<GameObject>(4);
 
 	[SerializeField]
-	private GameObject playerRigPrefab;
+	private readonly GameObject playerRigPrefab;
 
 	private ChaseCamera playerCamera;
 	private ChaseCamera2D playerCamera2D;
