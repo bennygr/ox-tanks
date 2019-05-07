f0eb916aee3e627a2203e05a8de593023f578ccc
Benni <benni@dhcp48.oxoe.int>
2019-05-07 14:32:31 +0200

    Added first POC loading screen

---
+++ OX Tanks/Assets/Scripts/UI/Menu/StartScreenHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenHandler : MonoBehaviour {
+
+
+
+    private void LoadGameScene(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Sandbox");
+    }
+
+    /// <summary>
+    /// Event Handler if "BtnFight" is clicked
+    /// </summary>
+    public void OnFightButtonClicked(){
+        Debug.Log("FIGHT button clicked");
+        LoadGameScene();
+    }
+}
